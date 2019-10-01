5a43d9e1911c6ecb69b40ede57be54590a6050c9
billyhoomm <billyhoom@gmail.com>
2019-10-01 23:09:36 +0800

    bug fix

--- AFront/src/views/blog.nav.vue
+++ AFront/src/views/blog.nav.vue
@@ -5,7 +5,7 @@
       <a v-if="isMobile && this.$route.name=='article'" @click="navBack" class="nav__item" title="后退">
         <i class="fa fa-arrow-left  fa-fw fa-lg blue blackBG"></i>
       </a>
-      <a v-else class="nav__item" @click="clearSessionStorage()" data-toggle="tooltip" data-placement="right" title="billyh" href = "#">
+      <a v-else class="nav__item" @click="clearSessionStorage()" data-toggle="tooltip" data-placement="right" title="billyhu" href = "#">
         <img class="siteLogo img-circle" src = "https://cdn.billyhu.com/icon/node.png">
       </a>
       <router-link class="nav__item" :to="{ name: 'index'}" exact data-toggle="tooltip" activeClass="active"
